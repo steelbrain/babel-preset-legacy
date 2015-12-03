bbbd54229405603db79f648431d9babb7fe6d93a
steelbrain <aneesiqbalbhatti@gmail.com>
2015-12-03 07:03:20 -0700

    :new: Add babel-plugin-syntax-object-rest-spread transformation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
-    require('babel-plugin-transform-es2015-arrow-functions')
+    require('babel-plugin-transform-es2015-arrow-functions'),
+    require('babel-plugin-syntax-object-rest-spread')
   ]
 }
